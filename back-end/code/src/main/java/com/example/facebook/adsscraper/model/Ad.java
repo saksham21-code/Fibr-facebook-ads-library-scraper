423f09e813680fafe71f9dc2e4a1509ac293930e
Saksham Tiwari <saksham21.work@gmail.com>
2024-09-20 19:13:00 +0530

    Update Ad.java

--- back-end/code/src/main/java/com/example/facebook/adsscraper/model/Ad.java
+++ back-end/code/src/main/java/com/example/facebook/adsscraper/model/Ad.java
@@ -11,11 +11,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Ad {
-    private String title;             // Title of the ad
-    private String description;       // Description of the ad
+    private String title;             // Title 
+    private String description;       // Description 
     private String media;             // Media link (image/video)
     private String accountName;       // Name of the Facebook account
-    private String libraryID;         // Unique identifier for the ad in the library
-    private String fbLink;            // Facebook link for the ad/account
-    private String igLink;            // Instagram link for the ad/account
+    private String libraryID;         
+    private String fbLink;            // Facebook link 
+    private String igLink;            // Instagram link 
 }
