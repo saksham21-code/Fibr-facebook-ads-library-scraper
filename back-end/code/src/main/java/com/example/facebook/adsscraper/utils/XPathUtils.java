4569e4da84518a4788d58d36b06fbacd8169d39f
Saksham Tiwari <saksham21.work@gmail.com>
2024-09-21 12:28:43 +0530

    Update XPathUtils.java

--- back-end/code/src/main/java/com/example/facebook/adsscraper/utils/XPathUtils.java
+++ back-end/code/src/main/java/com/example/facebook/adsscraper/utils/XPathUtils.java
@@ -1,1 +1,11 @@
+package com.example.facebook.adsscraper.utils;
+
+public class XPathUtils {
+
+    // dynamically generate the country option XPath based on country name
+    public static String getCountryOptionXPath(String country) {
+        return "//div[text()='" + country + "']";
+    }
+
+}
 
